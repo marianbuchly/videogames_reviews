478d815d2eab781dfdc3c39d9410c5db7abf2b6c
marianbuchly <marianbuchly@gmail.com>
2016-04-28 12:30:21 +0200

    seeds game

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 print "seeding categories"
 
-Category.create([
-  { name: "PC Games"},
-  { name: "XBOX Games"},
-  { name: "Nintendo Games"},
-  { name: "Playstation Games"}
+pc_games = Category.create( name: "PC Games")
+xbox_games = Category.create( name: "XBOX Games")
+nintendo_games = Category.create( name: "Nintendo Games")
+playstation_games = Category.create( name: "Playstation Games")
 
-  ])
+puts "seeded #{Category.count} categories"
+print "seeding some games"
 
-  puts "seeded #{Category.count} categories"
+Game.create(title: "Pong", category: pc_games)
+Game.create(title: "Zelda", category: nintendo_games)
+Game.create(title: "Mario", category: nintendo_games)
+Game.create(title: "Sonic", category: playstation_games)
+Game.create(title: "X", category: xbox_games)
+Game.create(title: "Pacman", category: pc_games)
+
+puts "seeded #{Game.count} games"
